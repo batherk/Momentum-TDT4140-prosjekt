3d1c648347c137ce10b85cae1d4923265091f43b
Bjørn Are Therkelsen <ba.therk@outlook.com>
2019-03-12 16:07:17 +0100

    Add positions-view on endpoint api/mycompanies/slug/positions

--- backend/api/views/company_owner.py
+++ backend/api/views/company_owner.py
@@ -3,6 +3,9 @@ from api.models.company import *
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
+from rest_framework.response import Response
+from ..models.position import Position
+from ..serializers.position import PositionSerializerDepth0
 
 import api.permissions as custom_perm
 
@@ -17,3 +20,8 @@ class CompanyOwnerView(viewsets.ModelViewSet):
     def get_queryset(self):
         return Company.objects.filter(owner=self.request.user)
 
+    @action(url_path='positions', detail=True)
+    def positions(self, request, slug):
+        positions_queryset = Position.objects.filter(company__slug=slug)
+        data = PositionSerializerDepth0(positions_queryset, context={'request': self.request}, many=True).data
+        return Response(data)
